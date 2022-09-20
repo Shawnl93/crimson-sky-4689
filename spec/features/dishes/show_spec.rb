cafb3beb406f9810aca499a2ddcbe7c0d3db1c45
Shawn Lee <Shawnl93@hotmail.com>
2022-09-20 16:02:04 -0600

    got stuck on the routes for wayyyy to long

---
+++ spec/features/dishes/show_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'the dishes show page' do
+
+  it "shows dishes name and description" do
+    chef_1 = Chef.create(name: "Shawn")
+    dish = Dish.create(name: "Orange Chicken", description: "Orange flavored chicken", chef_id: chef_1.id)
+    dish_2 = Dish.create(name: "Beef and Brocolli", description: "Beef flavored Brocolli")
+    visit "/dishes/#{dish.id}"
+  end
+
+  it "list of ingredients for that dish"
+
+  it "name of chef's"
+end
